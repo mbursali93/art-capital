3b3184f8f166c1af1b114056d43918bc604b44c6
Mustafa Bursali <m.bursali16@gmail.com>
2023-04-07 23:18:55 +0300

    validation added for user-service

--- user-service/src/routes/auth-route.ts
+++ user-service/src/routes/auth-route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import UserController from "../controller/user-controller";
+import userValidation from "../middlewares/validators";
 
 const router = Router()
 const controller = new UserController()
 
 
-router.post("/register", controller.register)
+router.post("/register", userValidation, controller.register)
 router.post("/login", controller.login)
 router.post("/logout", controller.logout)
 router.post("/refresh", controller.refreshToken)
